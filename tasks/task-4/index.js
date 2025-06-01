8679fe3301fbf6962cb5bc825fa03e23204e9d35
Kirill <urmancheev.it@gmail.com>
2025-06-02 00:20:38 +0300

    fix/removed console, corrected the result of the function, earlier on each iteration I received a character from each line, now I add a character on each iteration.

--- tasks/task-4/index.js
+++ tasks/task-4/index.js
@@ -1,12 +1,9 @@
 const reverseStringOne = (string) => {
   let resultString = '';
   for (let i = string.length - 1; i >= 0; i--) {
-    // let res = string.slice(0 + i - 1);
-    resultString = string[i];
-
-    console.log(resultString);
+    resultString += string[i];
   }
   return resultString;
 };
-// reverseStringOne('hello');
+
 console.log(reverseStringOne('hello'));
